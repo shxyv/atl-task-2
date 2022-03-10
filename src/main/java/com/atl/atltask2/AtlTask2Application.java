ade26436bafe4808d589c86411209f66cf230bc5
shxyv <ilgar.shixiyevv@gmail.com>
2022-03-10 18:40:33 +0400

    Added changelog files

---
+++ src/main/java/com/atl/atltask2/AtlTask2Application.java
@@ -0,0 +1,13 @@
+package com.atl.atltask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlTask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atl/atltask2/Atltask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atl.atltask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atltask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
